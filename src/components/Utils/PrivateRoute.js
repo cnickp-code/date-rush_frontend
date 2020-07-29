043b8e684c877988b7e2d4c07c8778c9882c4e54
Carlo Paredes <c.nick.paredes@gmail.com>
2020-07-29 00:33:28 -0700

    Added public and private route components

---
+++ src/components/Utils/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import TokenService from '../../services/token-service';
+
+export default function PrivateRoute({ component, ...props }) {
+    const Component = component
+    return (
+        <Route
+            {...props}
+            render={renderProps => (
+                TokenService.hasAuthToken()
+                    ? <Component {...renderProps} />
+                    : <Redirect
+                        to={{
+                            pathName: '/signup',
+                            state: { from: renderProps.location }
+                        }} />
+            )}
+        />
+    )
+}
---
+++ src/components/Utils/PublicRoute.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import TokenService from '../../services/token-service'
+
+export default function PublicRoute({ component, ...props }) {
+    const Component = component
+    return (
+        <Route
+            {...props}
+            render={renderProps => (
+                TokenService.hasAuthToken()
+                    ? <Redirect to={'/signup'} />
+                    : <Component {...renderProps} />
+            )}
+        />
+    )
+}
